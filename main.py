c3ad5aac1da9a23490e88a93ae39dcbfe979384d
Hal <waikei1917oct@gmail.com>
2017-03-16 16:32:07 -0600

    Crawler

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from spider import Spider
 from domain import *
 from general import *
 
-PROJECT_NAME = 'thenewboston'
-HOMEPAGE = 'https://thenewboston.com/'
+PROJECT_NAME = 'mydrivers'
+HOMEPAGE = 'http://www.mydrivers.com/'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
