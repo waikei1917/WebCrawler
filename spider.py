c1aea51ef5c98ccb330081144c1ab0e0a5fed433
Hal <waikei1917oct@gmail.com>
2017-03-16 16:05:27 -0600

    Crawler

--- spider.py
+++ spider.py
@@ -36,7 +36,7 @@ class Spider:
         if page_url not in Spider.crawled:
             print(thread_name + ' now crawling ' + page_url)
             print('Queue ' + str(len(Spider.queue)) + ' | Crawled ' + str(len(Spider.crawled)))
-            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.add_links_to_queue(Spider.gather_links(page_url))
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
             Spider.update_files()
