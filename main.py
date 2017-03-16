ef9c255e5f2ebc04ee342c5e6f5401f83392af4b
Hal <waikei1917oct@gmail.com>
2017-03-16 16:23:38 -0600

    Crawler

--- main.py
+++ main.py
@@ -13,3 +13,29 @@ NUMBER_OF_THREADS = 8
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
+def create_workers():
+    for t in range(NUMBER_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon = True
+        t.start()
+
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+        queue.join()
+        crawl()
+
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) < 0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
+
+create_workers()
+crawl()
